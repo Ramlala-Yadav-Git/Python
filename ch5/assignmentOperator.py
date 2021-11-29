09181b2f6901ee59390f142ec45e6227389a2893
Ramlala Yadav <easyexam01@gmail.com>
2021-11-29 13:04:46 +0530

    started python course added assgnment operator chapter

---
+++ ch5/assignmentOperator.py
@@ -0,0 +1,12 @@
+# assignment operators in python
+a = 15
+a += 15  # addition
+s = 15
+s -= 15  # substraction operator
+m = 15
+m *= 15  # multiplication
+d = 15
+d /= 15  # division
+
+
+print(a, s, m, d)
